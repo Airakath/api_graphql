9908fa12e896c40e92ec10e8ff013c9a835195e2
Nicolas Bernin <nbernin@chez-nestor.com>
2020-10-21 11:53:38 +0200

    add fake db

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const { ApolloServer } = require('apollo-server');
+const db = require('./includes/courses');
 
 const typeDefs = ` 
     type Query {
         hello: String!
+        totalCourses: Int!
     }
 `;
 
 const resolvers = {
-    Query: {
-        hello: () => "Hello l'API fonctionne !"
-    }
+	Query: {
+		hello: () => "Hello l'API fonctionne !",
+		totalCourses: () => db.cours.length
+	},
 };
 
 const server = new ApolloServer({
